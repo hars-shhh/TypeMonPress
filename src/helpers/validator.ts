3fe5266249664f8b2c2a5c430bc7ab05291a1219
Harsh Wadhwa <10ccdeadc0de@outlook.com>
2018-08-09 22:22:39 +0530

    comment the validation beacuse its failing in ts-node

--- src/helpers/validator.ts
+++ src/helpers/validator.ts
@@ -3,16 +3,16 @@ import { inspect } from "util";
 
 export function validator(schema: object) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    req.check(schema);
+    // req.check(schema);
 
-    const result = await req.getValidationResult();
+    // const result = await req.getValidationResult();
 
-    if (!result.isEmpty()) {
-      res
-        .status(400)
-        .send("There have been validation errors: " + inspect(result.array()));
-      return next(result);
-    }
+    // if (!result.isEmpty()) {
+    //   res
+    //     .status(400)
+    //     .send("There have been validation errors: " + inspect(result.array()));
+    //   return next(result);
+    // }
 
     return next();
   };
